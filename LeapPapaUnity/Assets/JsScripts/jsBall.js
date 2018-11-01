2b2b93489e25a5ce36c420c1a0e4518ef00d6da0
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2018-11-01 14:43:12 +0900

    recommit
    recommit

--- LeapPapaUnity/Assets/JsScripts/jsBall.js
+++ LeapPapaUnity/Assets/JsScripts/jsBall.js
@@ -1,7 +1,44 @@
 ﻿#pragma strict
 
-static var speed = 5.5;
+static var MinimumSpeed = 25;
+static var MaximumSpeed = 30;
 
+static var MinimumVerticalMovement = 0.1F;
+private var hasBeenLaunched = true;
+
+// function Update() {
+//     if(hasBeenLaunched) {
+//         var ball: GameObject = GameObject.Find("Ball");
+
+//         var direction = ball.GetComponent.<Rigidbody>().velocity;
+//         var speed = direction.magnitude;
+//         direction.Normalize();
+
+//         if (direction.z > -MinimumVerticalMovement && direction.z < MinimumVerticalMovement) {
+//             if(direction.z < 0)
+//                 direction.z = -MinimumVerticalMovement;
+//             else
+//                 direction.z = MinimumVerticalMovement;
+
+
+//             if(direction.x < 0)
+//                 direction.x = -1 + MinimumVerticalMovement;
+//             else 
+//                 direction.x = 1 - MinimumVerticalMovement;
+
+//             //direction.z = direction.z < 0 ? -MinimumVerticalMovement : MinimumVerticalMovement;
+//             //direction.x = direction.x < 0 ? -1 + MinimumVerticalMovement : 1 - MinimumVerticalMovement;
+
+//             ball.GetComponent.<Rigidbody>().velocity = direction * speed;
+//         }
+
+//         if(speed < MinimumSpeed || speed > MaximumSpeed) {
+//             speed = Mathf.Clamp(speed, MinimumSpeed, MaximumSpeed);
+
+//            ball.GetComponent.<Rigidbody>().velocity = direction * speed;
+//         }
+//     }
+// }
 
 
 function OnCollisionEnter(coll: Collision) { 
@@ -12,19 +49,24 @@ function OnCollisionEnter(coll: Collision) {
         if(jsGameManager.state != STATE.DEMO) 
             coll.collider.gameObject.SendMessage("SetCollision", flag, SendMessageOptions.DontRequireReceiver);
      
-        //Physics.gravity.z -= 0.2;
+        
         //if (Physics.gravity.x > 0) {
         //    Physics.gravity.x -= 6;
         //} else {
         //    Physics.gravity.x += 6;
         //}
     }
+    if(coll.collider.gameObject.tag == "BLOCK1" || coll.collider.gameObject.tag == "FENCE")
+        Physics.gravity.z -= 0.2;
+
+    if(coll.collider.gameObject.tag == "PADDLE") {
+        var ball: GameObject = GameObject.Find("Ball");
+        ball.GetComponent.<Rigidbody>().AddForce(Vector3.forward * 100);
+
+    }
 
 }
 
-function Bounce(coll: Collider) {
-
-}
 
 function CheckBounds(coll: Collider) {
     if (coll.tag == "CEILING") {
---
+++ LeapPapaUnity/Assets/JsScripts/jsPaddle.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+private var speed = 10;
+private var ball : GameObject;
+
+
+function Start() {
+    ball = GameObject.Find("Ball");
+}
+
+function Update () {
+	
+}
+
+function OnTriggerEnter(coll : Collider) {
+
+    if(coll.gameObject.tag == "BALL") {
+        //coll.transform.rotation.eulerAngled
+    }
+}
