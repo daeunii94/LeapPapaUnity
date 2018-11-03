cd91e3f5db4dc6dc08a34fbdfdf5e8d56f76fefc
daeunii94 <39181911+daeunii94@users.noreply.github.com>
2018-11-03 22:31:05 +0900

    constraint on ball velocity
     - constraint on ball velocity by 7
    
     - Addforce when hitting walls and floor
    <floor>
    velocity.z is positive -> give force forward + up
    
    velocity.z is negative-> give force back + up
    
    <left wall>
    velocity.z is positive -> give force forward + right
    
    velocity.z is negative -> give force back + right
    
    <right wall>
    velocity.z is positive-> give force forward + left
    
    velocity.z is negative-> give force back + left

--- LeapPapaUnity/Assets/JsScripts/jsBall.js
+++ LeapPapaUnity/Assets/JsScripts/jsBall.js
@@ -1,5 +1,6 @@
 ﻿#pragma strict
 
+//var ball : GameObject;
 
 static var MinimumSpeed = 25;
 static var MaximumSpeed = 30;
@@ -7,6 +8,29 @@ static var MaximumSpeed = 30;
 static var MinimumVerticalMovement = 0.1F;
 private var hasBeenLaunched = true;
 
+function Update() {
+    
+    print("공 속도: " + gameObject.GetComponent.<Rigidbody>().velocity);
+    //print("각속도 벡터: " + gameObject.GetComponent.<Rigidbody>().angularVelocity);
+    if(gameObject.GetComponent.<Rigidbody>().velocity.x > 8) 
+        gameObject.GetComponent.<Rigidbody>().velocity.x = 8;
+    
+    if(gameObject.GetComponent.<Rigidbody>().velocity.y > 8)
+        gameObject.GetComponent.<Rigidbody>().velocity.y = 8;
+
+    if(gameObject.GetComponent.<Rigidbody>().velocity.z > 8)
+        gameObject.GetComponent.<Rigidbody>().velocity.z = 8;
+
+        
+    if(gameObject.GetComponent.<Rigidbody>().velocity.x < -8) 
+        gameObject.GetComponent.<Rigidbody>().velocity.x = -8;
+
+    if(gameObject.GetComponent.<Rigidbody>().velocity.y < -8)
+        gameObject.GetComponent.<Rigidbody>().velocity.y = -8;
+
+    if(gameObject.GetComponent.<Rigidbody>().velocity.z < -8)
+        gameObject.GetComponent.<Rigidbody>().velocity.z = -8;
+}
 
 
 // function Update() {
@@ -64,21 +88,75 @@ function OnCollisionEnter(coll: Collision) {
         //}
     }
     if(coll.collider.gameObject.tag == "BLOCK1" || coll.collider.gameObject.tag == "FENCE")
-        Physics.gravity.z -= 0.2;
+        //Physics.gravity.z -= 0.12;
 
-    if(coll.collider.gameObject.tag == "PADDLE") {
-        var ball: GameObject = GameObject.Find("Ball");
-        ball.GetComponent.<Rigidbody>().AddForce(Vector3.forward * 100);
+    
+    print("이름1:"+coll.collider.gameObject.name);
+    if(coll.collider.gameObject.name == "forearm") {
+        //print("패들태그1:" + coll.collider.gameObject.tag);
+        //var ball: GameObject = GameObject.Find("Ball");
+        //print("오브젝트: " + ball.gameObject.name);
+        print("들어옴1");
 
+        print("오브젝트1:" + gameObject.name);
+        gameObject.GetComponent.<Rigidbody>().AddForce(Vector3.forward * 200);
+
+
+        //gameObject.GetComponent.<Rigidbody>().AddForce(force, ForceMode.Impulse);
+    }
+
+    if(coll.collider.gameObject.tag == "FLOOR") {
+        print("땅친당");
+        if(gameObject.GetComponent.<Rigidbody>().velocity.z > 0)
+           gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(0,1,1) * 100);
+        else
+            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(0,1,-1) * 100);
+    }
+
+    if(coll.collider.gameObject.name == "Left Wall") {
+        print("z방향1:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
+        if(gameObject.GetComponent.<Rigidbody>().velocity.z > 0) {
+            print("z방향3:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
+            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(1,0,1) * 100);
+            
+        }
+        else {
+            print("z방향3:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
+            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(1,0,-1) * 100);
+            
+        }
+        //print("왼쪽 벽 쳤을때 속도 +-? :" +  gameObject.GetComponent.<Rigidbody>().velocity);
+        
+        print("왼쪽 벽 침!");
+        //gameObject.GetComponent.<Rigidbody>().AddForce(Vector3.right * 100);
+        
     }
     
+    if(coll.collider.gameObject.name == "Right Wall") {
+        print("z방향1:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
+        if(gameObject.GetComponent.<Rigidbody>().velocity.z > 0) {
+            print("z방향3:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
+            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(-1,0,1) * 150);
+            
+        }
+        else {
+            print("z방향3:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
+            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(-1,0,-1) * 150);
+            
+        }
+        //print("왼쪽 벽 쳤을때 속도 +-? :" +  gameObject.GetComponent.<Rigidbody>().velocity);
+        
+        print("오른쪽 벽 침!");
+        //gameObject.GetComponent.<Rigidbody>().AddForce(Vector3.right * 100);
+        
+    }
    
 }
 
 
 function CheckBounds(coll: Collider) {
     if (coll.tag == "CEILING") {
-
+        
         return;
 
     }
