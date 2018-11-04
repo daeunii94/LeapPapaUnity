5fd4229cd39547066dc1e21d680615f44ef55049
dj <joanne3467@naver.com>
2018-11-04 21:07:16 +0900

    Revert "Merge branch 'countScore' of https://github.com/daeunii94/LeapPapaUnity into countScore"
    This reverts commit 8b2da3046fe30e4b4db2939a60f8d125405f2924, reversing
    changes made to a10decaed2add21696774f78edb4bb47ba543126.

--- LeapPapaUnity/Assets/JsScripts/jsBall.js
+++ LeapPapaUnity/Assets/JsScripts/jsBall.js
@@ -1,6 +1,5 @@
 ﻿#pragma strict
 
-//var ball : GameObject;
 
 static var MinimumSpeed = 25;
 static var MaximumSpeed = 30;
@@ -8,34 +7,6 @@ static var MaximumSpeed = 30;
 static var MinimumVerticalMovement = 0.1F;
 private var hasBeenLaunched = true;
 
-function Update() {
-    
-    print("공 속도: " + gameObject.GetComponent.<Rigidbody>().velocity);
-    //print("각속도 벡터: " + gameObject.GetComponent.<Rigidbody>().angularVelocity);
-    if(gameObject.GetComponent.<Rigidbody>().velocity.x > 8) 
-        gameObject.GetComponent.<Rigidbody>().velocity.x = 8;
-    
-    if(gameObject.GetComponent.<Rigidbody>().velocity.y > 8)
-        gameObject.GetComponent.<Rigidbody>().velocity.y = 8;
-
-    if(gameObject.GetComponent.<Rigidbody>().velocity.z > 8)
-        gameObject.GetComponent.<Rigidbody>().velocity.z = 8;
-
-        
-    if(gameObject.GetComponent.<Rigidbody>().velocity.x < -8) 
-        gameObject.GetComponent.<Rigidbody>().velocity.x = -8;
-
-    if(gameObject.GetComponent.<Rigidbody>().velocity.y < -8)
-        gameObject.GetComponent.<Rigidbody>().velocity.y = -8;
-
-    if(gameObject.GetComponent.<Rigidbody>().velocity.z < -8)
-        gameObject.GetComponent.<Rigidbody>().velocity.z = -8;
-
-    if(gameObject.GetComponent.<Rigidbody>().velocity.z == 0.0) 
-        gameObject.GetComponent.<Rigidbody>().AddForce(Vector3.forward * 70);
-
-    //if(gameObject.GetComponent.<Rigidbody>().)
-}
 
 
 // function Update() {
@@ -93,75 +64,21 @@ function OnCollisionEnter(coll: Collision) {
         //}
     }
     if(coll.collider.gameObject.tag == "BLOCK1" || coll.collider.gameObject.tag == "FENCE")
-        //Physics.gravity.z -= 0.12;
+        Physics.gravity.z -= 0.2;
 
-    
-    print("이름1:"+coll.collider.gameObject.name);
-    if(coll.collider.gameObject.name == "forearm") {
-        //print("패들태그1:" + coll.collider.gameObject.tag);
-        //var ball: GameObject = GameObject.Find("Ball");
-        //print("오브젝트: " + ball.gameObject.name);
-        print("들어옴1");
+    if(coll.collider.gameObject.tag == "PADDLE") {
+        var ball: GameObject = GameObject.Find("Ball");
+        ball.GetComponent.<Rigidbody>().AddForce(Vector3.forward * 100);
 
-        print("오브젝트1:" + gameObject.name);
-        gameObject.GetComponent.<Rigidbody>().AddForce(Vector3.forward * 300);
-
-
-        //gameObject.GetComponent.<Rigidbody>().AddForce(force, ForceMode.Impulse);
-    }
-
-    if(coll.collider.gameObject.tag == "FLOOR") {
-        print("땅친당");
-        if(gameObject.GetComponent.<Rigidbody>().velocity.z > 0)
-           gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(0,1,1) * 100);
-        else
-            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(0,1,-1) * 100);
-    }
-
-    if(coll.collider.gameObject.name == "Left Wall") {
-        print("z방향1:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
-        if(gameObject.GetComponent.<Rigidbody>().velocity.z > 0) {
-            print("z방향3:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
-            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(1,0,1) * 100);
-            
-        }
-        else {
-            print("z방향3:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
-            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(1,0,-1) * 100);
-            
-        }
-        //print("왼쪽 벽 쳤을때 속도 +-? :" +  gameObject.GetComponent.<Rigidbody>().velocity);
-        
-        print("왼쪽 벽 침!");
-        //gameObject.GetComponent.<Rigidbody>().AddForce(Vector3.right * 100);
-        
     }
     
-    if(coll.collider.gameObject.name == "Right Wall") {
-        print("z방향1:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
-        if(gameObject.GetComponent.<Rigidbody>().velocity.z > 0) {
-            print("z방향3:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
-            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(-1,0,1) * 150);
-            
-        }
-        else {
-            print("z방향3:"+gameObject.GetComponent.<Rigidbody>().velocity.z);
-            gameObject.GetComponent.<Rigidbody>().AddForce(Vector3(-1,0,-1) * 150);
-            
-        }
-        //print("왼쪽 벽 쳤을때 속도 +-? :" +  gameObject.GetComponent.<Rigidbody>().velocity);
-        
-        print("오른쪽 벽 침!");
-        //gameObject.GetComponent.<Rigidbody>().AddForce(Vector3.right * 100);
-        
-    }
    
 }
 
 
 function CheckBounds(coll: Collider) {
     if (coll.tag == "CEILING") {
-        
+
         return;
 
     }
